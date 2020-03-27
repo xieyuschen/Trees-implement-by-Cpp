da327e6d358b1bb09c689c000e2280e50267ce82
Xieyuschen <Xieyuschen@users.noreply.github.com>
2020-03-27 23:35:03 +0800

    Add readme and find errors in codes which had been seen as demo.

--- B-Tree/main.cpp
+++ B-Tree/main.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
-#include"BinarySearchTree.h"
+#include"DemoBSTree.h"
 #include<vector>
 #include<set>
 using namespace std;
 int main() {
-	Bstree t{ 3,1,2,4,5 };
-	cout << t.empty() << endl;;
+
+	BSTree<int> tint;
 	for (int i = 1; i < 6; i++) {
-		cout << t.Remove(i);
-
+		cout << tint.insert(i);
 	}
-
-
+	for (int i = 1; i < 6; i++) {
+		tint.Remove(tint.Min());
+	}
 }
